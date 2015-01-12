56501e8204bba81de01fac52fa4b95c37422387a
Jacob Niedzwiecki <jake@jacob-n.com>
2015-01-12 09:36:26 -0500

    Initial Commit

---
+++ beacontest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beacontest
+//
+//  Created by Jacob Niedzwiecki on 2015-01-12.
+//  Copyright (c) 2015 Jacob Niedzwiecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ beacontest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  beacontest
+//
+//  Created by Jacob Niedzwiecki on 2015-01-12.
+//  Copyright (c) 2015 Jacob Niedzwiecki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
